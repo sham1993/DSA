f090585e765d00c28b38320c5071b16bfbdcb577
254001 <254001@INNOITWF2025KG9.AMERICAS.GLOBAL.NTTDATA.COM>
2024-07-11 18:04:17 +0530

    bubble sort updated

--- DSA/src/BubbleSort.java
+++ DSA/src/BubbleSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		int arr[] = { 5, 4, 3, 2, 1 };
+		int arr[] = { 0,8,2,6,4,5 };
 
 		sort(arr);
 		System.out.println(Arrays.toString(arr));
@@ -11,17 +11,21 @@ public class BubbleSort {
 
 	private static void sort(int[] arr) {
 		// 3 2 4 5
-
+           
 		for (int i = 0; i < arr.length; i++) {
-
-			for (int j = 0; j < arr.length - 1 - i; j++) {
+			boolean isSwapped=false;
+			for (int j = 1; j < arr.length - 1 - i; j++) {
 
 				if (arr[j + 1] < arr[j]) {
 					swap(j + 1, j, arr);
+					isSwapped=true;
 				}
 
 			}
-			// System.out.println("run loop : "+i+ " " + Arrays.toString(arr));
+			if(!isSwapped) {
+				break;
+			}
+			 System.out.println("run loop : "+i+ " " + Arrays.toString(arr));
 		}
 
 	}
